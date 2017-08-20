679bcd89cbad8465fa39cd9d46e0ed09c9ede092
Grégory <gregory_bevan@hotmail.com>
2017-08-20 09:47:03 +0200

    Prepare step 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
@@ -12,6 +11,8 @@ export default {
 </script>
 
 <style>
+@import '~bootstrap/dist/css/bootstrap.min.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
