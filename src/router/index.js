bda9ba84e82fa64416c578b5fb729e2ea2cf0709
Grégory Bévan <gregory_bevan@hotmail.com>
2017-09-20 21:35:59 +0200

    End of step 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Favorites from '@/components/Favorites'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/favorites',
+      name: 'Favorites',
+      component: Favorites
     }
   ]
 })
