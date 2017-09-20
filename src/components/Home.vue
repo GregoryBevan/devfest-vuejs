a6a67c4309b0487804587d2962b740de061f0bd5
Grégory Bévan <gregory_bevan@hotmail.com>
2017-09-20 21:17:15 +0200

    End of step 2

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ helloWorld }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            helloWorld: 'Hello World !'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
