fb99765b8b50e4409165b7635fec31317120ab7c
Grégory Bévan <gregory_bevan@hotmail.com>
2017-09-21 22:54:31 +0200

    End of step 5

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>
-        {{ helloWorld }}
+    <div class="container">
+        <ul>
+            <serie></serie>
+            <serie></serie>
+            <serie></serie>
+            <serie></serie>
+        </ul>
     </div>
 </template>
 
 <script>
+import Serie from '@/components/Serie.vue'
+
 export default {
     data() {
         return {
             helloWorld: 'Hello World !'
         }
+    },
+    components: {
+        serie: Serie
     }
 }
 </script>
---
+++ src/components/Serie.vue
@@ -0,0 +1,41 @@
+<template>
+    <li class="list-group-item">
+        <div class="media">
+            <div class="media-left">
+                <img class="media-object" :src="image.medium" />
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading">{{ name }}</h4>
+                <div>{{ summary }}</div>
+            </div>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'NightClazz',
+            summary: 'What an amazing codelab …',
+            image: {
+                medium: 'static/logo.png'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.list-group-item {
+    position: relative
+}
+
+.media {
+    text-align: left;
+}
+
+.media-object {
+    height: 64px;
+}
+</style>
