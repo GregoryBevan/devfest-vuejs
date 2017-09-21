d1e57e81f55101ee9376adce153ebe999b4923ca
Grégory Bévan <gregory_bevan@hotmail.com>
2017-09-21 21:50:24 +0200

    End of step 4

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    navbar: Navbar
+  }
 }
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand">
+          <router-link to="/">Mes Séries</router-link>
+        </a>
+      </div>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <router-link to="/">Accueil</router-link>
+        </li>
+        <li>
+          <router-link to="/favorites">Favorites</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
