8320d1e0a348f0f51fff3c41082c14e7eeb85ac3
Franck <franck.abgrall@zenika.com>
2017-09-25 18:45:19 +0200

    Comments changed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,15 +3,15 @@
     <div class="container-fluid">
       <div class="navbar-header">
         <a class="navbar-brand">
-          <!-- Lien vers la page principale -->
+          <!-- Lien vers la page "liste des séries" -->
         </a>
       </div>
       <ul class="nav navbar-nav navbar-right">
         <li>
-          <!-- Lien vers la page principale -->
+          <!-- Lien vers la page "liste des séries" -->
         </li>
         <li>
-          <!-- Lien vers la page principale -->
+          <!-- Lien vers la page "favoris" -->
         </li>
       </ul>
     </div>
