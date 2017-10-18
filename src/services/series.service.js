060575102436e6bf62cf9d66ac702cbfee754d52
gregory <gregory_bevan@hotmail.com>
2017-10-18 14:39:21 +0200

    Use fetch instead of axios as http client

--- src/services/series.service.js
+++ src/services/series.service.js
@@ -1,7 +1,3 @@
-import axios from 'Axios'
-
 export default {
-    getSeries () {
-        return axios.get('http://api.tvmaze.com/search/shows?q=bad')
-    }
+	getSeries: () => fetch('http://api.tvmaze.com/search/shows?q=bad').then(res => res.json())
 }
