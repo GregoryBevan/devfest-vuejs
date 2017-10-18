d5cc8fa30ac7febaad223a4d8888f69dc1e8837f
gregory <gregory_bevan@hotmail.com>
2017-10-18 14:23:31 +0200

    Use fetch instead of axios as http client

--- src/services/series.service.js
+++ src/services/series.service.js
@@ -1,6 +1,3 @@
-//import axios from 'axios';
-
 export default {
-	// TODO: Créer la fonction pour récupérer les séries !
-	// http://api.tvmaze.com/search/shows?q=[insert_your_keyword]
+	getSeries: () => fetch('http://api.tvmaze.com/search/shows?q=bad').then(res => res.json())
 }
